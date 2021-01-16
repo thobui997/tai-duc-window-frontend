8be9723b503aeaca5b7369f48fb779f3dcc9dfaa
ThoBT <thob2105@gmail.com>
2021-01-16 14:30:50 +0700

    change file environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  api_url: 'http://localhost:1337',
+  api_url: 'https://taiducwindow.herokuapp.com',
   production: false
 };
 
