06fb653d5b887f5d11cb80433bb5143eb975deff
ThoBT <thob2105@gmail.com>
2021-03-30 20:49:42 +0700

    add image for our services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,24 +15,28 @@ import { IntroduceService } from '../api/introduce/introduce.service';
 export class HomeComponent implements OnInit {
   ourService = [
     {
+      image: '../../assets/images/vận chuyển.jpg',
       title: 'Thi công lắp đặt',
       body:
-        'Với đội ngũ nhiều năm kinh nghiệm chuyên lắp đặt các hệ cửa cao cấp như cửa Hyundai Hàn Quốc, cửa nhôm Xingfa, cửa nhôm Schuco, cửa nhôm PMI, cửa nhôm Topalu, cửa nhôm Germany, cửa nhôm JMA, Cửa nhôm cầu cách nhiệt, Cửa nhôm PMA, Cửa nhựa lõi thép... Taiducwindow.com luôn đảm bảo tốt tiến độ, kỹ thuật và thẩm mỹ cao.',
+        'Với đội ngũ nhiều năm kinh nghiệm chuyên lắp đặt các hệ cửa cao cấp như cửa Hyundai Hàn Quốc, cửa nhôm Xingfa, cửa nhôm Schuco, cửa nhôm PMI, cửa nhôm Topalu, cửa nhôm Germany, cửa nhôm JMA, Cửa nhôm cầu cách nhiệt, Cửa nhôm PMA, Cửa nhựa lõi thép... Tài Đức luôn đảm bảo tốt tiến độ, kỹ thuật và thẩm mỹ cao.',
     },
     {
+      image: '../../assets/images/phong thủy.jpg',
       title: 'Tư vấn thiết kế - phong thủy',
       body:
         'Trong Xây dựng nói chung và đặc biệt lắp đặt các loại cửa làm thế nào để có thể ngăn chặn và làm giảm sự kích thích không mong muốn từ bên ngoài, lấy năng lượng sống, thải ra khí cũ và tiếp nhận không khí mới, Do đó phong thuỷ trong vấn đề lắp đặt cửa rất quan trọng đối với các công trình hay ngôi nhà, không nên bỏ qua.',
     },
     {
+      image: '../../assets/images/hợp tác.jpg',
       title: 'Phân phối nhôm và phụ kiện',
       body:
-        'Taiducwindow.com chuyên cung cấp các Profile nhôm cao cấp như Hyundai, Xingfa, Schuco, JMA, các loại phụ kiện cao cấp như Cmech, Kinlong, Ace (Hàn Quốc) , các hệ cửa tự động.',
+        'Tài Đức chuyên cung cấp các Profile nhôm cao cấp như Hyundai, Xingfa, Schuco, JMA, các loại phụ kiện cao cấp như Cmech, Kinlong, Ace (Hàn Quốc) , các hệ cửa tự động.',
     },
     {
+      image: '../../assets/images/gia công.jpg',
       title: 'Gia công và sản xuất',
       body:
-        'Với nhà xưởng và máy móc hiện đại, Taiducwindow.com đảm bảo khả năng sản xuất các hệ cửa cao cấp đúng quy trình, kỹ thuật và độ thậm mỹ cao, tiến độ tốt, sản lượng lớn.',
+        'Với nhà xưởng và máy móc hiện đại, Tài Đức đảm bảo khả năng sản xuất các hệ cửa cao cấp đúng quy trình, kỹ thuật và độ thậm mỹ cao, tiến độ tốt, sản lượng lớn.',
     },
   ];
   introduce: IntroduceModel;
